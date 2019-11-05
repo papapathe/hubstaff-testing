6c79916b066e67acd6493112874a553cd66b9eac
Alex Yarotsky <yarotsky.alex@gmail.com>
2019-11-05 12:34:29 +0300

    Add project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_092454) do
+ActiveRecord::Schema.define(version: 2019_11_05_093051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,13 @@ ActiveRecord::Schema.define(version: 2019_11_05_092454) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.bigint "organization_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organization_id"], name: "index_projects_on_organization_id"
+  end
+
+  add_foreign_key "projects", "organizations"
 end
