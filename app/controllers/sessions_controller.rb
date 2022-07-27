0cea6a8faa1920a5a39f9920913913399e542642
Pathe SENE <pathe.sene@gmail.com>
2022-07-26 19:47:13 -0500

    ISSUE_ID fix rubocop warnings

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
+# Handles user session creation
 class SessionsController < ApplicationController
-  def create
-  end
+  def create; end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to have_secure_password}
+  it { is_expected.to have_secure_password }
 end
