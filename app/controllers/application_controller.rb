53431b328ad43f42ebd81368c0fa8d3a596b0393
Pathe SENE <pathe.sene@gmail.com>
2022-07-26 19:44:59 -0500

    ISSUE_ID ensure we authenticate the user before processing any request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@
 # Base class for all controllers
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  attr_reader :current_user
+
+  before_action :authenticate_with_token!
 
   private
+
+  def authenticate_with_token!
+    @current_user = Authenticator.new.authenticate!(request.headers['Authorization'])
+  end
 
   def not_found
     render json: { error: 'Not Found' }, status: :not_found
