b9cb48dcbfd7cd217c27348a45fd332b9566f02a
Pathe SENE <pathe.sene@gmail.com>
2022-07-26 19:45:56 -0500

    ISSUE_ID add a shared context that creates a jwt for testing endpoints

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -19,8 +19,20 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+RSpec.shared_context 'when user logged in' do
+  let(:user) { create(:user) }
+  let(:jwt) { JwtService.new.encode(user: user, algorithm: 'HS256', secret: 'secret') }
+  let(:token) { MessageEncryptorService.new(jwt).encrypt }
+
+  before do
+    request.headers['Authorization'] = token
+  end
+end
+
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include_context 'when user logged in', type: :controller
 end
